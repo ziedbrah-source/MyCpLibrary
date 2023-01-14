45d087ac7b3de982c0205b94255409c0dc7ac6e7
ziedbrah-source <ziedbrahmi2@gmail.com>
2023-01-14 18:25:45 +0100

    improved template of Binomial coefficient O(n+log(p)) where P is the mod

--- combinatorics.cpp
+++ combinatorics.cpp
@@ -1,57 +1,41 @@
-#include <bits/stdc++.h>
-using namespace std;
+const int MAXN = 1e6;
 
-typedef long long ll;
-
-const int MAX_N = 100010;
-const int p = 1e9+7;                             // p is a prime > MAX_N
-
-ll mod(ll a, int m) {                            // returns a (mod m)
-  return ((a%m) + m) % m;                        // ensure positive answer
+long long fac[MAXN + 1], inv[MAXN + 1];
+		
+/** Computes x^y modulo p in O(log p) time. */
+long long exp(long long x, long long y, long long p) {
+	long long res = 1; x %= p;
+	while (y) {
+		if (y & 1) {
+			res *= x; res %= p; 
+		}
+		x *= x; x %= p;
+		y >>= 1;
+	}
+	return res;
 }
 
-ll modPow(ll b, int p, int m) {                  // assume 0 <= b < m
-  if (p == 0) return 1;
-  ll ans = modPow(b, p/2, m);                    // this is O(log p)
-  ans = mod(ans*ans, m);                         // double it first
-  if (p&1) ans = mod(ans*b, m);                  // *b if p is odd
-  return ans;                                    // ans always in [0..m-1]
+/** Precomputes n! from 0 to MAXN. */
+void factorial(long long p) {
+	fac[0] = 1;
+	for (int i = 1; i <= MAXN; i++) {
+		fac[i] = fac[i - 1] * i % p;
+	}
 }
 
-ll inv(ll a) {                                   // Fermat's little theorem
-  return modPow(a, p-2, p);                      // modPow in Section 5.8
-}                                                // that runs in O(log p)
-
-ll fact[MAX_N], invFact[MAX_N];
-
-ll C(int n, int k) {                             // O(log p)
-  if (n < k) return 0;                           // clearly
-  return (((fact[n] * inv(fact[k])) % p) * inv(fact[n-k])) % p;
-  // return (((fact[n] * invFact[k]) % p) * invFact[n-k]) % p; // O(1)
+/** Precomputes all modular inverse factorials from 0 to MAXN in O(n + log p) time */
+/** we use the fact that inv(n!)=inv(n!)*(inv(n+1)*n+1)=inv((n+1)!)*n+1 
+ * (we use this because computing inverse factorias 
+ * online can be costly (if we add log every time.. why not once right? :p)
+ * */
+void inverses(long long p) {
+	inv[MAXN] = exp(fac[MAXN], p - 2, p);
+	for (int i = MAXN; i >= 1; i--) {
+		inv[i - 1] = inv[i] * i % p;
+	}
 }
-
-ll Fib[MAX_N], Cat[MAX_N];
-
-int main() {
-  Fib[0] = 0;                                    // the first
-  Fib[1] = 1;                                    // two terms of Fibonacci
-  for (int i = 2; i < MAX_N; ++i)                // O(MAX_N) pre-processing
-    Fib[i] = (Fib[i-1] + Fib[i-2]) % p;          // Fib[i] in [0..p-1]
-  cout << Fib[100000] << "\n";                   // the answer is 911435502
-
-  fact[0] = 1;
-  for (int i = 1; i < MAX_N; ++i)                // O(MAX_N) pre-processing
-    fact[i] = (fact[i-1]*i)% p;                  // fact[i] in [0..p-1]
-
-  // invFact[MAX_N-1] = inv(fact[MAX_N-1]);         // one O(log p)
-  // for (int i = MAX_N-2; i >= 0; --i)             // O(MAX_N) pre-processing
-  //   invFact[i] = (invFact[i+1]*(i+1)) % p;
-  cout << C(100000, 50000) << "\n";              // the answer is 149033233
-
-  Cat[0] = 1;
-  for (int n = 0; n < MAX_N-1; ++n)              // O(MAX_N * log p)
-    Cat[n+1] = ((4*n+2)%p * Cat[n]%p * inv(n+2)) % p;
-  cout << Cat[100000] << "\n";                   // the answer is 945729344
-
-  return 0;
+		
+/** Computes nCr mod p */
+long long choose(long long n, long long r, long long p) {
+	return fac[n] * inv[r] % p * inv[n - r] % p;
 }
