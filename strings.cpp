1efd98d164aa0cf92104fa84292d029af4edf2a1
ziedbrah-source <ziedbrahmi2@gmail.com>
2023-01-20 19:08:25 +0100

    update strings hash

--- strings.cpp
+++ strings.cpp
@@ -1,32 +1,36 @@
+using ll = long long;
+
 class HashedString {
-	private:
-		// change M and B if you want
-		static const long long M = 1e9 + 9;
-		static const long long B = 9973;
+  private:
+	// change M and B if you want
+	static const ll M = (1LL << 61) - 1;
+	static const ll B;
 
-		// pow[i] contains B^i % M
-		static vector<long long> pow;
+	// pow[i] contains B^i % M
+	static vector<ll> pow;
 
-		// p_hash[i] is the hash of the first i characters of the given string
-		vector<long long> p_hash;
-	public:
-		HashedString(const string& s) : p_hash(s.size() + 1) {
-			while (pow.size() < s.size()) {
-				pow.push_back((pow.back() * B) % M);
-			}
+	// p_hash[i] is the hash of the first i characters of the given string
+	vector<ll> p_hash;
 
-			p_hash[0] = 0;
-			for (int i = 0; i < s.size(); i++) {
-				p_hash[i + 1] = ((p_hash[i] * B) % M + s[i]) % M;
-			}
+	__int128 mul(ll a, ll b) { return (__int128)a * b; }
+	ll mod_mul(ll a, ll b) { return mul(a, b) % M; }
+
+  public:
+	HashedString(const string &s) : p_hash(s.size() + 1) {
+		while (pow.size() < s.size()) {
+			pow.push_back(mod_mul(pow.back(), B));
 		}
-
-		long long getHash(int start, int end) {
-			long long raw_val = (
-				p_hash[end + 1] - (p_hash[start] * pow[end - start + 1])
-			);
-			return (raw_val % M + M) % M;
+		p_hash[0] = 0;
+		for (int i = 0; i < s.size(); i++) {
+			p_hash[i + 1] = (mul(p_hash[i], B) + s[i]) % M;
 		}
+	}
+
+	ll getHash(int start, int end) {
+		ll raw_val =
+			p_hash[end + 1] - mod_mul(p_hash[start], pow[end - start + 1]);
+		return (raw_val + M) % M;
+	}
 };
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 vector<ll> HashedString::pow = {1};
@@ -39,3 +43,4 @@ const ll HashedString::B = uniform_int_distribution<ll>(0, M - 1)(rng);
  * like this problem : https://dmoj.ca/problem/ccc20s3
  * I used the hash in a set because I got an MLE when I insert whole string :)
  * GLHF!
+ */
