ebcf76f629c55c3c7841eaeeb79545deb54b2296
ziedbrah-source <ziedbrahmi2@gmail.com>
2023-06-15 18:29:42 +0100

    problem C

---
+++ TCPC2022/problemC_sieve_firstPrimeDivisor.cpp
@@ -0,0 +1,127 @@
+// WE ARE NOT HERE TO TAKE PART , WE ARE HERE TO TAKE OVER
+#include <bits/stdc++.h>
+using namespace std;
+#define fastInp cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define         aff(v) for(auto e:v) cout<<e<<" ";cout<<endl;
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define all(x)            (x).begin(),(x).end()
+#define sz(x)             (int)((x).size())
+typedef long long ll;
+typedef long double ld;
+ll n,q,d;
+const ll prime=1e9+7;
+const ll prime2=998244353;
+const ll prime3=7901;
+
+const ll MOD = 998244353;
+ll INF=2e18;
+const int NAX=1e7+5;
+vector<int> sieve(NAX); // kol element bech thotlou asgher prime ye9smou.
+void init(){
+	
+	sieve[1]=1;
+	for(int i=2;i<NAX;i++){
+		if(sieve[i]==0){
+			sieve[i]=i;
+			for(int j=i+i;j<NAX;j+=i){
+				if(sieve[j]==0) sieve[j]=i;
+			
+			}
+		}
+
+	}
+
+}
+void solve(){
+	cin >> n;
+	vector<int> A(n);
+	vector<int> B(n);
+	for(int i=0;i<n;i++){
+		cin >> A[i];
+	}
+	for(int i=0;i<n;i++){
+		cin >> B[i];
+	}
+	int times=0;
+	for(int i=0;i<n;i++){
+		while(A[i]!=B[i]){
+			if(A[i]>B[i]){
+				times++;
+				A[i]=A[i]/sieve[A[i]];
+			}else{
+				times++;
+				B[i]=B[i]/sieve[B[i]];
+			}
+		}
+	}
+	cout << times << "\n";
+
+	
+}
+	
+	
+
+ 
+int main() {
+	fastInp;
+	//init();
+	//debug() << imie(s);
+	//freopen("grids.in","r",stdin);
+	//freopen("res.out","w",stdout);
+	// __gcd <long long> (x, y);
+	int tc=1;
+	//debug() << imie(sieve);
+	cin >> tc;
+	//cout << setprecision(10)<<fixed;
+	init();
+	while(tc--){
+		//i++;
+		//cout <<"Test " << i << ":" << "\n";
+		
+		solve();
+		
+	}
+
+	return 0;
+}
+
+/*
+   Some insights:
+    .Binary search
+    .Graph representation
+    .Write brute force code
+    .Change your approach
+ 
+ 
+ */
+
+
