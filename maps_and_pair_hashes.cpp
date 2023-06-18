dcc10dfd417a3ff2fdd0d358abac3b934f34c953
ziedbrah-source <ziedbrahmi2@gmail.com>
2023-06-18 22:17:06 +0100

    add hashes to pairs in maps

---
+++ maps_and_pair_hashes.cpp
@@ -0,0 +1,17 @@
+truct custom_hash {
+	static uint64_t splitmix64(uint64_t x) {
+		// http://xorshift.di.unimi.it/splitmix64.c
+		x += 0x9e3779b97f4a7c15;
+		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+		return x ^ (x >> 31);
+	}
+
+	size_t operator()(pair<uint64_t,uint64_t> x) const {
+		static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+		return splitmix64(x.first + FIXED_RANDOM)^(splitmix64(x.second + FIXED_RANDOM) >> 1);
+	}
+};
+
+// usage
+//unordered_map<pair<int,int>,int,custom_hash> mp;
